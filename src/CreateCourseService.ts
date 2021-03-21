ea26a4a1d7dfc638a7c40f80ff12b9132ec1f7cf
Luciana Nunes <lcnunes09@gmail.com>
2021-03-21 15:27:43 -0300

    Create first route and Course Service

---
+++ src/CreateCourseService.ts
@@ -0,0 +1,8 @@
+class CreateCourseService {
+    
+    execute(name: string, duration: number, educator: string) {
+        console.log(name, duration, educator);
+    }
+}
+
+export default new CreateCourseService();
